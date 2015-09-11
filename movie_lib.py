694494ac3f1243f7deb78c99b37d87d7324ef018
Kathleen Rauh <krauh2010@gmail.com>
2015-09-11 10:27:17 -0400

    In progress commit.

--- movie_lib.py
+++ movie_lib.py
@@ -1,24 +1,39 @@
+all_movies = {} # movie objects
+all_users = {}  # user objects
 
 class Movie:
     ''' Class for movies '''
     def __init__(self, movie, title):
-        self.movie = movie
+        self.id = movie
         self.title = title
+        all_movies[self.id] = self
+        self.ratings = {}
+ #key is user_id, value is Rating object
+    def __str__(self):
+        return 'Movie(movie={}, title={})'.format(self.movie, self.title)
 
-    def get_movie_by_id(self, title):
-        movie_id:title
-        for movie_id in movie_dict
+    def __repr__(self):
+        return self__str__()
 
+    # def get_movie_by_id(self, title):
+    #     movie_id:title
+    #     for movie_id in movie_dict
+    def get_ratings(self):
+        return self.ratings.values()
+
+    def add_rating(self, rating):
+        self.ratings[rating.user_id] = rating
 
 class User:
     ''' Class for users '''
     def __init__(self, user):
-        self.user = user
+        self.id = user
+        all_users[self.id] = self
 
-    def get_rating_data(self, movie):
-        [[id[i]
-        for id in user_data_list]
-        for i in range (len(user_data_list[0]))
+    # def get_rating_data(self, movie):
+    #     [[id[i]
+    #     for id in user_data_list]
+    #     for i in range (len(user_data_list[0]))
 
 
 class Rating:
@@ -27,6 +42,14 @@ class Rating:
         self.user = user
         self.movie = movie
         self.stars = stars
+        all_movies[self.movie].add_rating(self)
+
+    def __str__(self):
+    return 'Movie(movie={}, title={})'.format(self.movie, self.title)
+        return
+
+    def __repr__(self):
+        return self__str__()
 
 #     def get_movie_rating(self, stars):
 #         self.mid = mid
--- test_movie_ratings.py
+++ test_movie_ratings.py
@@ -1,38 +1,77 @@
 
-from nose.tools import raises
-
-# Movie data
-movie_dict = {
-    1 : 'Toy Story',
-    2 : 'GoldenEye',
-    3 : 'Four Rooms',
-    4 : 'Get Shorty'
-}
-
-# User rating data
-# User Movie Rating TimeStamp
-user_data_list = (
-[196, 242, 3, 881250949],
-[186, 302, 3, 891717742],
-[22, 377, 1, 878887116],
-[244, 51, 2, 880606923],
-[166, 346, 1, 886397596])
-
-def test_get_movie_by_id():
-    assert get_movie_by_id(title) == 'Toy Story'
-    assert get_movie_by_id(title) != 'Toy Story'
-    assert get_movie_by_id(title) != ''
-
+from nose.tools import *
 #
-# def test_get_user_ratings():
-#     assert get_user_ratings(uid, stsrs) ==
+# # Movie data
+# movie_dict = {
+#     1 : 'Toy Story',
+#     2 : 'GoldenEye',
+#     3 : 'Four Rooms',
+#     4 : 'Get Shorty'
+# }
 #
-#
-# def test_get_movie_rating():
-#     assert get_movie_rating(stars) != ' '
+# # User rating data
+# # User Movie Rating TimeStamp
+# user_data_list = (
+# [196, 242, 3, 881250949],
+# [186, 302, 3, 891717742],
+# [22, 377, 1, 878887116],
+# [244, 51, 2, 880606923],
+# [166, 346, 1, 886397596])
+
+print(all_movies)
+
+print(all_movies[3])
+
+user1 = User(5)
+user2 = User(12)
+movie1 = Movie(3, 'Toy Story')
+movie2 = Movie(9, 'Pretty Woman')
+rating1 = Rating(user1.id, movie1.id, 4)
+rating2 = Rating(user2.id, movie2.id, 1)
+rating3 = Rating(user1.id, movie1.id, 3)
+rating4 = Rating(user2.id, movie1.id, 2)
+
+def test_user_creation():
+    assert User1.id == 5
+    assert User2.id == 12
+
+def test_movie_creation():
+    assert Movie1.id == 3
+    assert Movie1.title == 'Toy story'
+    assert Movie2.id == 9
+    assert Movie2.title == 'Pretty Woman'
+
+def test_rating_creation():
+    assertrating1.user.id == rating3.user.id == user1.id
+    assertrating2.user.id == rating4.user.id == user2.id
+    assert rating1.stars == 4
+    assert rating2.stars == 1
+    assert rating3.stars == 3
+    assert rating4.stars == 2
+
+def test_find_ratings_for_movie():
+    # return a list of Rating objects
+    toy_story_ratings = all_movies[movie1.id].get_ratings()
+    print len(toy_story_ratings)
+    print (toy_story_ratings)
+    assert len(toy_story_ratings) == 2
 
 
-def test_get_rating_data():
-    assert User.get_rating_data(movie) == '242'
-    assert get_rating_data(movie) != ''
-    assert get_rating_data(movie) != '196'
+# def test_get_movie_by_id():
+#     assert get_movie_by_id(title) == 'Toy Story'
+#     assert get_movie_by_id(title) != 'Toy Story'
+#     assert get_movie_by_id(title) != ''
+#
+# #
+# # def test_get_user_ratings():
+# #     assert get_user_ratings(uid, stsrs) ==
+# #
+# #
+# # def test_get_movie_rating():
+# #     assert get_movie_rating(stars) != ' '
+#
+#
+# def test_get_rating_data():
+#     assert User.get_rating_data(movie) == '242'
+#     assert get_rating_data(movie) != ''
+#     assert get_rating_data(movie) != '196'
